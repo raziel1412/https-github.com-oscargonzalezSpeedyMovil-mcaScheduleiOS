76408147207ac6500569aeb4cec49684fe87c655
Omar Israel Trujillo Osornio <otrujillo201513@gmail.com>
2018-12-27 11:17:16 -0600

    dev

---
+++ mcaScheduleiOS/mcaScheduleiOS.h
@@ -0,0 +1,19 @@
+//
+//  mcaScheduleiOS.h
+//  mcaScheduleiOS
+//
+//  Created by Omar Israel Trujillo Osornio on 12/27/18.
+//  Copyright © 2018 Speedy Movil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for mcaScheduleiOS.
+FOUNDATION_EXPORT double mcaScheduleiOSVersionNumber;
+
+//! Project version string for mcaScheduleiOS.
+FOUNDATION_EXPORT const unsigned char mcaScheduleiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mcaScheduleiOS/PublicHeader.h>
+
+
